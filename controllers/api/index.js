1535a144f2242b1881cc7264afbed7190cd24262
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-27 20:50:27 -0700

    Added main layout and homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const categoryRoutes = require('./categoryRoutes');
 
-router.use('/users', userRoutes);
-router.use('/categories', categoryRoutes);
+// router.use('/users', userRoutes);
+// router.use('/categories', categoryRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,18 @@
 // This is where tell the server what data and where to render it
+const router = require('express').Router();
+const { User, Category, Transactions } = require('../models');
+const { beforeFindAfterOptions } = require('../models/User');
+const withAuth = require('../utils/auth');
+
+module.exports = router;
+
+router.get('/', (req, res) => {
+
+    try {
+
+        res.render('homepage')
+    } catch(err) {
+        res.status(500).json(err);
+    }
+
+});
