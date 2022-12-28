7a91740feac41b017107687c2d9e7baaa9f21db2
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-27 19:27:33 -0700

    Refactored server and indexes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 // Boiler-plate code ***Will need to be updated!*** 
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const catgoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/categories', catgoryRoutes);
 
 module.exports = router;
