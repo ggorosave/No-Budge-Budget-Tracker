2c71ed69e0f638bb2bf86bbaf1f06340022650d8
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-28 17:58:07 -0700

    Deleted reference route in userRoute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,69 +3,6 @@ const router = require('express').Router();
 const { json } = require('express');
 const { User, Category, Transactions } = require('../../models');
 
-// DELETE - For Grant's reference only
-router.get('/', async (req, res) => {
-  try {
-    
-    const userData = await User.findAll({
-      include: [{ model: Transactions }]
-    });
-    
-    res.status(200).json(userData);
-  } catch(err) {
-    res.status(500).json(err)
-  }
-});
-
-router.get('/transactions', async (req, res) => {
-  try {
-    
-    const transactionData = await Transactions.findAll({
-      include: [{ model: Category }]
-    });
-
-    const transactions = await transactionData.map((category) => category.get({ plain: true }));
-    
-    console.log(transactions)
-    
-    res.status(200).json(transactionData);
-  } catch(err) {
-    res.status(500).json(err)
-  }
-});
-
-router.get('/categories', async (req, res) => {
-  try {
-    
-    const transactionData = await Category.findAll({
-      include: [
-        { 
-          model: Transactions,
-          where: {
-            user_id: 1
-          },
-          attributes: ['transaction_date', 'amount', 'item_name']
-        }
-      ],
-    });
-
-    const categories = await transactionData.map((category) => category.get({ plain: true }));
-    // const transactions = await transactionData.map((category) => category.transactions.map((transaction) => transaction.get({ plain: true })));
-    // const transactions = await transactionData.map((category) => category.transactions.map((transaction) => transaction.get({ plain: true })));
-
-    console.info(categories)
-    // console.log(transactions)
-    // console.log(newArr)
-
-    
-    
-    res.status(200).json(transactionData);
-  } catch(err) {
-    res.status(500).json(err)
-  }
-});
-// ^^^DELETE - For Grant's reference only^^^
-
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
