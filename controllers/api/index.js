9a8c490fc4f7632fe4f6f48e973b251d0e1fe451
Isabella <isabellapierce93@gmail.com>
2023-01-02 21:07:19 -0700

    Worked on transaction routes & tested on insomnia.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,13 @@
 // Boiler-plate code ***Will need to be updated!*** 
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const transactionRoutes = require('./transactionRoutes')
 // const categoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
 // router.use('/categories', categoryRoutes);
+
+router.use('/transactions', transactionRoutes)
+
 
 module.exports = router;
