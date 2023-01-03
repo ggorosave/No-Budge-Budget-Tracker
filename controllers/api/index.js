7c870e94b84c1c04ef51c87bdcced21af40ea60d
Isabella <isabellapierce93@gmail.com>
2023-01-02 21:17:21 -0700

    Worked on category routes & tested on insomnia.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const transactionRoutes = require('./transactionRoutes')
-// const categoryRoutes = require('./categoryRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/categories', categoryRoutes);
-
+router.use('/categories', categoryRoutes);
 router.use('/transactions', transactionRoutes)
 
 
--- controllers/api/transactionRoutes.js
+++ controllers/api/transactionRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Transactions } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
 // Post Route
 router.post('/', async (req, res) => {
